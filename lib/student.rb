1a1af91f15db98777e0c2b1d232e784ff7c75366
Steven Senkowski <scsenkowski@gmail.com>
2017-12-28 13:49:03 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -29,4 +29,18 @@ def self.drop_table
 
     DB[:conn].execute(sql)
 end
+
+def save
+  if self.id
+    self.update
+  else
+    sql = <<-SQL
+      INSERT INTO students (name, album)
+      VALUES (?, ?)
+    SQL
+    DB[:conn].execute(sql, self.name, self.album)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+  end
+end
+
 end
