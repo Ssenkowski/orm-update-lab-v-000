9bffa749b8ef27d0a32cf89ffd4243c5ba3e999e
Steven Senkowski <scsenkowski@gmail.com>
2017-12-28 13:56:55 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -49,8 +49,12 @@ def self.create(name:, grade:)
   student
 end
 
-def self.new_from_db(id, name, grade)
-
+def self.new_from_db(row)
+  new_student = self.new  
+  new_student.id = row[0]
+  new_student.name =  row[1]
+  new_student.length = row[2]
+  new_student  
 end
 
 def self.find_by_name(name)
